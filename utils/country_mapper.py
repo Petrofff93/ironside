7b29326ae7ab1bd985ba0925a144c7b1a8bc2368
Petar Petrov <ppetrov@roiti.com>
2022-10-12 12:15:30 +0300

    re-upload mapper

---
+++ utils/country_mapper.py
@@ -0,0 +1,15 @@
+mapper = {
+    "BE": "Belgium",
+    "ES": "Spain",
+    "ES_": "Spain*",
+    "FR": "France",
+    "GB_PRE_BREXIT": "Great Britain (Pre Brexit)",
+    "GB*": "Great Britain (Post Brexit)",
+    "GR": "Greece",
+    "HR": "Croatia",
+    "IT": "Italy",
+    "LT": "Lithuania",
+    "NL": "Netherlands",
+    "PL": "Poland",
+    "PT": "Protugal"
+}
